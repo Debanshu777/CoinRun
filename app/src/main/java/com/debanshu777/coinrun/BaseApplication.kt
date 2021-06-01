8e329987d92e0fe5d23b7cbe3866015b57302453
Debanshu Datta <debanshudatta123@gmail.com>
2021-06-01 21:15:00 +0530

    Added Dagger-Hilt and stater code

---
+++ app/src/main/java/com/debanshu777/coinrun/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.debanshu777.coinrun
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
--- app/src/main/java/com/debanshu777/coinrun/MainActivity.kt
+++ app/src/main/java/com/debanshu777/coinrun/MainActivity.kt
@@ -2,10 +2,20 @@ package com.debanshu777.coinrun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.debanshu777.coinrun.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("RunDAO","RunDAO:${runDAO.hashCode()}")
     }
 }
