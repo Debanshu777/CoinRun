dbb6c3b84ce2ad9c71c03b3b5465156f1ba1a3a8
Debanshu Datta <debanshudatta123@gmail.com>
2021-06-05 23:42:05 +0530

    Added Navigation added basic UI

--- app/src/main/java/com/debanshu777/coinrun/ui/MainActivity.kt
+++ app/src/main/java/com/debanshu777/coinrun/ui/MainActivity.kt
@@ -2,20 +2,29 @@ package com.debanshu777.coinrun.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.debanshu777.coinrun.R
 import com.debanshu777.coinrun.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO;
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener{_,destination,_ ->
+                when(destination.id){
+                    R.id.settingsFragment,R.id.runFragment,R.id.statisticsFragment->
+                        bottomNavigationView.visibility= View.VISIBLE
+                    else-> bottomNavigationView.visibility=View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/com/debanshu777/coinrun/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/debanshu777/coinrun/ui/fragments/RunFragment.kt
@@ -1,14 +1,24 @@
 package com.debanshu777.coinrun.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.debanshu777.coinrun.R
 import com.debanshu777.coinrun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel:MainViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment2)
+        }
+    }
 }
