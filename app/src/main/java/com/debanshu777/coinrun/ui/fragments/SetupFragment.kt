ce01b773135255a8860646eca0abac3ed35c4ee3
Debanshu Datta <debanshudatta123@gmail.com>
2021-06-06 16:42:50 +0530

    Added the Google map requirements

--- app/src/main/java/com/debanshu777/coinrun/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/debanshu777/coinrun/ui/fragments/SetupFragment.kt
@@ -1,21 +1,70 @@
 package com.debanshu777.coinrun.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.debanshu777.coinrun.R
+import com.debanshu777.coinrun.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.debanshu777.coinrun.other.Utility
 import kotlinx.android.synthetic.main.fragment_setup.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
-class SetupFragment : Fragment(R.layout.fragment_setup) {
+class SetupFragment : Fragment(R.layout.fragment_setup),EasyPermissions.PermissionCallbacks {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requestPermission()
         tvContinue.setOnClickListener{
             findNavController().navigate(R.id.action_setupFragment_to_runFragment)
         }
+    }
+
+    private fun requestPermission(){
+        if(Utility.hasLocationPermission(requireContext())){
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept this permission for the app to work",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }else{
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept this permission for the app to work",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        }else{
+            requestPermission()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override  fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
